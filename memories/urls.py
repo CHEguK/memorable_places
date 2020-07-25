36a4eeed6dcd0661958a7728ad5ed1a75197dd0d
G-Man <zdrogov.an@yandex.ru>
2020-07-25 09:31:34 +0500

    Create home page Update memorable_places/urls.py Update memories/urls.py Update memories/views.py Create memories/home.html

--- memories/urls.py
+++ memories/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = 'memories'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('memories.urls', namespace='memories')),
+    path('', views.home, name='home')
 ]

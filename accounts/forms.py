87f7ed969e7a5b54f479b48519f9f67d479a1744
G-Man <zdrogov.an@yandex.ru>
2020-07-26 12:24:24 +0500

    Add 'first name' to registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,7 +13,7 @@ class RegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ("username", "email")
+        fields = ("username", "email", "first_name")
 
         def clean_password2(self):
             cd = self.cleaned_data

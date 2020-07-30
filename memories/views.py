bd41d438096f2b1f623261703bb0429db2cc0669
G-Man <zdrogov.an@yandex.ru>
2020-07-30 15:14:02 +0500

    Update memories/views.py for pylint, pycodestyle, mypy Update pylintrc

--- memories/views.py
+++ memories/views.py
@@ -1,34 +1,47 @@
-from django.shortcuts import render, redirect
+''' memories/views.py '''
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.urls import reverse
-from memories.models import MemoryItem
-from memories.forms import MemoryItemForm
 from django.views import View
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import login_required
-from django.conf import settings
+
+from memories.forms import MemoryItemForm
+from memories.models import MemoryItem
+
 
 @login_required
-def index(request):
+def index(request):  # pylint: disable=unused-argument
+    ''' Index '''
     return HttpResponse('Ваши воспоминания Туть')
 
+
 class MemoriesListView(ListView):
-    queryset = MemoryItem.objects.all()
+    ''' Memories List View '''
+    queryset = MemoryItem.objects.all()  # pylint: disable=no-member
     context_object_name = 'memories'
     template_name = "memories/list.html"
 
     def get_queryset(self):
-        u = self.request.user
-        if u.is_anonymous:
+        ''' Get queryset '''
+        user = self.request.user
+        if user.is_anonymous:
             return []
-        return u.memories.all()
+        return user.memories.all()
+
 
 class MemoriesCreateView(View):
-    def my_render(self, request, form, **kwargs):
+    ''' Memories Create View '''
+    @classmethod
+    def my_render(cls, request, form, **kwargs):
+        ''' Render method '''
         return render(request, "memories/create.html", {"form": form,
                                                         "mapbox_token": kwargs['mapbox_token']})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
+        ''' Post '''
         form = MemoryItemForm(request.POST)
         if form.is_valid():
             new_memory = form.save(commit=False)
@@ -37,15 +50,20 @@ class MemoriesCreateView(View):
             return redirect(reverse(viewname="memories:list"))
         return self.my_render(request, form)
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
+        ''' Get '''
         form = MemoryItemForm()
         return self.my_render(request, form, mapbox_token=settings.MAPBOX_TOKEN)
 
+
 class MemoryDetailsView(DetailView):
+    ''' Memory Details View '''
     model = MemoryItem
     template_name = 'memories/details.html'
 
-def delete_memory(request, uid):
-    m = MemoryItem.objects.get(id=uid)
-    m.delete()
+
+def delete_memory(request, uid):  # pylint: disable=unused-argument
+    ''' Delete memory '''
+    memory = MemoryItem.objects.get(id=uid)  # pylint: disable=no-member
+    memory.delete()
     return redirect(reverse(viewname="memories:list"))

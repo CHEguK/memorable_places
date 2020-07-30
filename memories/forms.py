59de137a4ecbd5b3efa77e8f4781f3c34437a6ab
G-Man <zdrogov.an@yandex.ru>
2020-07-30 15:20:58 +0500

    Update memories/forms.py & models.py for pylint, pycodestyle, mypy

--- memories/forms.py
+++ memories/forms.py
@@ -1,9 +1,13 @@
+''' memories/forms.py '''
 from django import forms
+
 from memories.models import MemoryItem
 
 
 class MemoryItemForm(forms.ModelForm):
+    ''' Memory Form '''
     class Meta:
+        ''' Meta class '''
         model = MemoryItem
         fields = ('name', 'comment')
         labels = {"name": "Название", "comment": "Комментарий"}
--- memories/models.py
+++ memories/models.py
@@ -1,16 +1,20 @@
+''' memories/models.py '''
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
 
 class MemoryItem(models.Model):
+    ''' Memory Item '''
     name = models.CharField(max_length=64)
     comment = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE,
                               related_name='memories')
 
-    def __str__(self) -> str:
+    def __str__(self):
+        ''' Magic method '''
         return self.name
 
-    def get_absolute_url(self) -> str:
+    def get_absolute_url(self):
+        ''' Get absolute url '''
         return reverse("memories:details", args=[self.pk])

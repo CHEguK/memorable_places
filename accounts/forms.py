91ea487bb94361ecca6520e363484abb17829a2d
G-Man <zdrogov.an@yandex.ru>
2020-07-30 13:41:21 +0500

    Edit accounts/forms.py for pylint, pycodestyle

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,28 @@
-from django.contrib.auth.models import User
+''' accounts/forms.py '''
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
+    ''' Login Form '''
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
 class RegistrationForm(forms.ModelForm):
+    ''' Registration Form '''
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
     class Meta:
+        ''' Meta '''
         model = User
         fields = ("username", "email", "first_name")
         labels = {"username": 'Логин', "email": 'Email', "first_name": 'Имя'}
 
         def clean_password2(self):
-            cd = self.cleaned_data
-            if cd["password"] != cd["password2"]:
+            ''' Check password are the same '''
+            clean_data = self.cleaned_data  # pylint: disable=no-member
+            if clean_data["password"] != clean_data["password2"]:
                 raise forms.ValidationError("Пароли не совпадают")
-            return cd["password2"]
+            return clean_data["password2"]
